4e1b205210c83bb0fd2de0bb1c404516ba378d3e
arkantara <126410659+arkantara@users.noreply.github.com>
2025-07-30 17:46:57 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
 
 // Supabase setup
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  'https://jjieqhvfadoqkahpqdvl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqaWVxaHZmYWRvcWthaHBxZHZsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjU2MTE4MSwiZXhwIjoyMDY4MTM3MTgxfQ.lGGDQiSvem3sHEwFYhUQU4nvnM80TIOMdTTaa2LiBBo'
 );
 
 // OpenAI setup
